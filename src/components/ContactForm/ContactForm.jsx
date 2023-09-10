1c0b85db1be9d3c1c6100616692dfa36e0a2748b
Metaldetect <stas.klymenko@icloud.com>
2023-09-10 21:55:25 +0200

    Update ContactForm.jsx

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import React, { useEffect } from 'react';
+import React from 'react';
 import * as Yup from 'yup';
 import { Formik, Form, Field } from 'formik';
 import { nanoid } from 'nanoid';
