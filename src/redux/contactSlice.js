807987f4ef51ce213b5a690ed1c64f4c0ba97033
Metaldetect <stas.klymenko@icloud.com>
2023-09-11 22:02:13 +0200

    fix

--- src/redux/contactSlice.js
+++ src/redux/contactSlice.js
@@ -1,18 +1,18 @@
-import { createSlice } from '@reduxjs/toolkit';
+// import { createSlice } from '@reduxjs/toolkit';
 
-const contactSlice = createSlice({
-  name: 'contacts',
-  reducers: {
-    addContact: (state, action) => {
-      state.contacts = [...state.contacts, action.payload];
-    },
-    deleteContact: (state, action) => {
-      state.contacts = state.contacts.filter(
-        contact => contact.id !== action.payload
-      );
-    },
-  },
-});
+// const contactSlice = createSlice({
+//   name: 'contacts',
+//   reducers: {
+//     addContact: (state, action) => {
+//       state.contacts = [...state.contacts, action.payload];
+//     },
+//     deleteContact: (state, action) => {
+//       state.contacts = state.contacts.filter(
+//         contact => contact.id !== action.payload
+//       );
+//     },
+//   },
+// });
 
-export const { addContact, deleteContact } = contactSlice.actions;
-export const contactsReducer = contactSlice.reducer;
+// export const { addContact, deleteContact } = contactSlice.actions;
+// export const contactsReducer = contactSlice.reducer;
